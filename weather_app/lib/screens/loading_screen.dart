82df17c028bf049699952d3598b4a1cdb2c0f83e
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-07-29 13:36:54 -0600

    add geolocator

--- weather_app/lib/screens/loading_screen.dart
+++ weather_app/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -9,14 +10,24 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Get Location'),
-        ),
-      ),
+    return const Scaffold(
+     
     );
   }
 }
